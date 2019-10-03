162290e5144aee25427c89a11db34aab95218478
Jay Schwan <jay.schwan@gmail.com>
2019-10-02 21:22:43 -0700

    added requirements, app.py, and jupyter notebook with basic sqlite engine

---
+++ app.py
@@ -0,0 +1,17 @@
+# import necessary libraries
+from flask import Flask, render_template, jsonify
+import requests
+import json
+
+# create instance of Flask app
+app = Flask(__name__)
+
+
+# create route that renders index.html template
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

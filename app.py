be624e9ac21d33205ed90c8e96a1a73cbc8d76d8
Jay Schwan <jay.schwan@gmail.com>
2019-10-05 10:16:21 -0700

    pushing some changes

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 # import necessary libraries
 from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import requests
 import json
 
 # create instance of Flask app
 app = Flask(__name__)
-
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:postgres@localhost:5432/malaria_db'
+db = SQLAlchemy(app)
 
 # create route that renders index.html template
 @app.route("/")
 def index():
     return render_template("index.html")
 
+#create a route for the data
+@app.route("/api")
+def api():
+    results =  
+    return jsonify(SOMETHING)
 
 if __name__ == "__main__":
     app.run(debug=True)

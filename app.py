0edd647821cc9de3f7dd36b46914131b779178be
Jay Schwan <jay.schwan@gmail.com>
2019-10-06 18:27:20 -0700

    updated flask app and requirements file

--- app.py
+++ app.py
@@ -3,58 +3,42 @@ import numpy as np
 import datetime
 import sqlalchemy
 import json
-from dateutil.relativedelta import relativedelta
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, Column, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String
+from flask import Flask, jsonify, request, render_template
 
-
-from flask import Flask, jsonify
-
-# # Database Setup
-# POSTGRES = {
-#     'user': 'postgres',
-#     'pw': 'postgres',
-#     'db': 'malria_db',
-#     'host': 'localhost',
-#     'port': '5432',
-# }
-
-engine = create_engine("postgresql://postgres:postgres@localhost:5432/malaria_db")
-
-
+engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/malaria_db")
 
 # reflect an existing database into a new model
 Base = automap_base()
 
-# reflect the tables
 Base.prepare(engine, reflect=True)
 
 # references to each table
-malaria_db = Base.classes.table_000817
+Table_000817 = Base.classes.table_000817
 
 # create instance of Flask app
 app = Flask(__name__)
 
 # create route that renders index.html template
-@app.route("/home", methods=['GET'])
-def index():
+@app.route("/home")
+def home():
     return render_template("home.html")
 
 # create route that renders index.html template
-@app.route("/main", methods=['GET'])
-def index():
+@app.route("/main")
+def main():
     return render_template("main.html")
 
 # create route that renders index.html template
-@app.route("/plots", methods=['GET'])
-def index():
+@app.route("/plots")
+def plots():
     return render_template("plots.html")
 
 #create a route for the data
-@app.route("/api", methods=['GET'])
+@app.route("/api")
 def api():
     # Create our session (link) from Python to the DB
     session = Session(engine)
@@ -62,36 +46,36 @@ def api():
     """Return a list of all results"""
     # Query all malaria
     results = session.query(
-        malaria_db.country,
-        malaria_db.code,
-        malaria_db.year,
-        malaria_db.under_5,
-        malaria_db.age_5_14,
-        malaria_db.age_15_49,
-        malaria_db.age_50_69,
-        malaria_db.over_70,
-        malaria_db.total_deaths,
-        malaria_db.populuation,
-        malaria_db.perc_of_pop_w_malaria,
-        malaria_db.gdp_per_capita).all()
+        Table_000817.country,
+        Table_000817.code,
+        Table_000817.year,
+        Table_000817.under_5,
+        Table_000817.age_5_14,
+        Table_000817.age_15_49,
+        Table_000817.age_50_69,
+        Table_000817.over_70,
+        Table_000817.total_deaths,
+        Table_000817.populuation,
+        Table_000817.perc_of_pop_w_malaria,
+        Table_000817.gdp_per_capita).all()
 
     session.close()
 
     # Create a dictionary from the row data and append to a list
     malaria_db = []
-    for country, code, year, under_5, age_5_14, age_15_49, age_50_69, over_70, total_deaths, populuation, perc_of_pop_w_malaria, gdp_per_capita    in results:
+    for country, code, year, under_5, age_5_14, age_15_49, age_50_69, over_70, total_deaths, populuation, perc_of_pop_w_malaria, gdp_per_capita in results:
         malaria_dict = {}
-        malaria_dict[country] = country
-        malaria_dict[code] = code
-        malaria_dict[year] = year
-        malaria_dict[under_5] = under_5
-        malaria_dict[age_5_14] = age_5_14
-        malaria_dict[age_15_49] = age_15_49
-        malaria_dict[age_50_69] = age_50_69
-        malaria_dict[over_70] = over_70
-        malaria_dict[total_deaths] = total_deaths
-        malaria_dict[populuation] = populuation
-        malaria_dict[perc_of_pop_w_malaria] = perc_of_pop_w_malaria
+        malaria_dict["country"] = country
+        malaria_dict["code"] = code
+        malaria_dict["year"] = year
+        malaria_dict["under_5"] = under_5
+        malaria_dict["age_5_14"] = age_5_14
+        malaria_dict["age_15_49"] = age_15_49
+        malaria_dict["age_50_69"] = age_50_69
+        malaria_dict["over_70"] = over_70
+        malaria_dict["total_deaths"] = total_deaths
+        malaria_dict["populuation"] = populuation
+        malaria_dict["perc_of_pop_w_malaria"] = perc_of_pop_w_malaria
         malaria_db.append(malaria_dict)
     return jsonify(malaria_db)
 
